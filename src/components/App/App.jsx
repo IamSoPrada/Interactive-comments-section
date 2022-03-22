f9f8a4c925ef7b1f3520b54889da38611f760c8d
Rustem Tarasevich <tarasevicrustem@gmail.com>
2022-03-22 20:36:02 +0300

    Added comment section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,14 +8,14 @@ function App() {
     <div className='bg-slate-100 mx-auto py-16'>
       <Container>
         <div className='bg-slate-50 relative flex flex-col-reverse sm:flex-row justify-between p-7 max-w-3xl rounded-md gap-6 shadow-lg'>
-          <div className='flex  sm:flex-col'>
+          <div className='flex sm:flex-col'>
             <button
               type='button'
-              className='px-4 py-1 text-purple-400 text-xl font-bold bg-slate-200 hover:bg-slate-300 rounded-l-lg sm:rounded-bl-none sm:rounded-tr-lg  cursor-pointer text-center'
+              className='px-4 py-1 text-purple-400 text-xl font-bold bg-slate-200 transition ease-in-out hover:bg-slate-300 rounded-l-lg sm:rounded-bl-none sm:rounded-tr-lg  cursor-pointer text-center'
             >
               +
             </button>
-            <span className='bg-slate-200 flex p-2 justify-center items-center text-purple-400 text-center align-middle font-bold'>
+            <span className='bg-slate-200 flex p-2 justify-center items-center transition ease-in-out text-purple-400 text-center align-middle font-bold'>
               18
             </span>
             <button
@@ -51,6 +51,37 @@ function App() {
             </p>
           </div>
           <div />
+        </div>
+
+        <div className='bg-slate-50 flex flex-col-reverse sm:flex-row justify-between p-7 max-w-3xl w-full rounded-md gap-6 shadow-lg'>
+          <div className='flex w-full flex-col sm:flex-row gap-4 justify-between items-start'>
+            <div className='hidden sm:block rounded-full w-14 h-10 '>
+              <img src={avatarNicole} alt='' />
+            </div>
+            <textarea
+              className='text-gray-600 w-full p-4 text-bold text-md border-2 border-gray-400 transition ease-in-out m-0 rounded-md  focus:text-gray-700 focus:bg-white focus:border-purple-300 focus:outline-none'
+              placeholder='Add comment'
+              rows='3'
+            />
+            <button
+              type='button'
+              className='hidden sm:block bg-purple-800  text-gray-50 rounded-md px-6 py-3 font-bold'
+            >
+              <span>Reply</span>
+            </button>
+            <div className='flex w-full gap-4 justify-between sm:hidden'>
+              <div className='rounded-full w-8 h-8 bg-slate-500'>
+                <img src={avatarNicole} alt='' />
+              </div>
+
+              <button
+                type='button'
+                className='bg-purple-800 block  text-gray-50 rounded-md px-6 py-3 font-bold'
+              >
+                <span>Reply</span>
+              </button>
+            </div>
+          </div>
         </div>
       </Container>
     </div>
--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Container({ children }) {
-  return <div className='container flex flex-col items-center px-4 min-h-screen'>{children}</div>;
+  return <div className='container flex flex-col items-center px-4 min-h-screen gap-4'>{children}</div>;
 }
 
 export default Container;
