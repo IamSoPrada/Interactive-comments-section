112dd58be774d0f95d6fc5cd989d36ac1d9b056b
Rustem Tarasevich <tarasevicrustem@gmail.com>
2022-03-31 21:49:35 +0300

    Removed bg from avatar

--- src/components/Cards/common components/Avatar.jsx
+++ src/components/Cards/common components/Avatar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Avatar({ avatar, classes }) {
   return (
-    <div className={`${classes} rounded-full  bg-slate-500`}>
+    <div className={`${classes} rounded-full`}>
       <img src={avatar} alt='' />
     </div>
   );
--- src/components/ReplyContainer/ReplyContainer.jsx
+++ src/components/ReplyContainer/ReplyContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ReplyContainer({ children }) {
   return (
-    <div className='flex relative flex-col pl-16 gap-4 max-w-3xl '>
+    <div className='flex relative flex-col pl-12 gap-4 max-w-3xl '>
       <div className='absolute w-1 left-6 h-full bg-gray-300' />
       {children}
     </div>
